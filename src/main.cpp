39baba42942576e5a6ee3eacce63fe2e591bb222
Stuart Bourne <sbourne7510@gmail.com>
2019-09-02 21:01:45 -0400

    Refactored showing windows into a single function

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,7 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgcodecs.hpp>
 
-const int MAX_VALUE = 255;
-const int MIN_VALUE = 0;
-const int BLUE_THRESH = 100;
-
-const char* RGB_WINDOW = "RGB_Window";
-const char* HSV_WINDOW = "HSV_Window";
-const char* BLUE_WINDOW = "BLUE_Window";
+void showWindows(std::initializer_list<cv::Mat> windows);
 
 int main(int argc, char** argv){
     std::cout << "Hello world" << std::endl;
@@ -20,9 +14,6 @@ int main(int argc, char** argv){
         std::cout << "Cannot read the input source image." << std::endl;
         return -1;
     }
-    cv::namedWindow(RGB_WINDOW, cv::WINDOW_AUTOSIZE);
-    cv::namedWindow(HSV_WINDOW, cv::WINDOW_AUTOSIZE);
-    cv::namedWindow(BLUE_WINDOW, cv::WINDOW_AUTOSIZE);
     
     //we will need to convert to HSV coordinates to allow for easier color detection
     cv::Mat hsv_board;
@@ -37,14 +28,24 @@ int main(int argc, char** argv){
     for (unsigned int i = 0; i < contours.size(); ++i){
         cv::drawContours(cont_drawing, contours, i, cv::Scalar(100, 100, 50));
     }
-
-    cv::imshow(RGB_WINDOW, rgb_board);
-    cv::imshow(HSV_WINDOW, hsv_board);
-    cv::imshow(BLUE_WINDOW, blue_board);
-    cv::imshow("contours", cont_drawing);
-    const int windowSize = cv::getWindowImageRect(RGB_WINDOW).size().width;
-    cv::moveWindow(HSV_WINDOW, windowSize, 0);
-    cv::moveWindow(BLUE_WINDOW, 2*windowSize, 0);
+    showWindows({rgb_board, hsv_board, blue_board, cont_drawing});
+    // cv::imshow(RGB_WINDOW, rgb_board);
+    // cv::imshow(HSV_WINDOW, hsv_board);
+    // cv::imshow(BLUE_WINDOW, blue_board);
+    // cv::imshow("contours", cont_drawing);
+    // const int windowSize = cv::getWindowImageRect(RGB_WINDOW).size().width;
+    // cv::moveWindow(HSV_WINDOW, windowSize, 0);
+    // cv::moveWindow(BLUE_WINDOW, 2*windowSize, 0);
     cv::waitKey(0);
     return 0;
+}
+
+void showWindows(std::initializer_list<cv::Mat> windows){
+    unsigned int i = 0;
+    for (auto window : windows){
+        cv::imshow(std::to_string(i), window);
+        const int windowSize = cv::getWindowImageRect(std::to_string(i)).size().width;
+        cv::moveWindow(std::to_string(i), windowSize * i, 0);
+        ++i;
+    }
 }
